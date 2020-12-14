2be6e1cf29636736a1fa2fc2070de46337c7ec49
aspxcor <aspxcor@gmail.com>
2020-12-15 05:55:46 +0800

    完成原始demo定稿

--- GAEvacuationPlan.py
+++ GAEvacuationPlan.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 import geatpy as ea  # import geatpy
 import numpy as np
+import random
 B=3
 R=3
 S=3
 T=3
-d=[[6,7,8],
+d=np.array([[6,7,8],
    [10,9,2],
-   [6,3,7]]
-dStart=[7,4,9]
-L=[1,3,3]
-U=[4,4,1]
+   [6,3,7]])
+dStart=np.array([7,4,9])
+L=np.array([1,3,3])
+U=np.array([4,4,1])
 Nind=40
-# t=[0,0,0]
-# Xijbr=np.zeros((3,3,3,3), dtype=np.int)
-# tMax=0
 
 class MyProblem(ea.Problem):  # 继承Problem父类
     def __init__(self):
         name = 'MyProblem'  # 初始化name（函数名称，可以随意设置）
         M = 1  # 初始化M（目标维数）
         maxormins = [1]  # 初始化maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）
-        Dim = 81  # 初始化Dim（决策变量维数）
+        Dim = B*R*S*T  # 初始化Dim（决策变量维数）
         varTypes = [1] * Dim  # 初始化varTypes（决策变量的类型，元素为0表示对应的变量是连续的；1表示是离散的）
         lb = [0] * Dim # 决策变量下界
         ub = [1]  * Dim# 决策变量上界
@@ -34,12 +32,12 @@ class MyProblem(ea.Problem):  # 继承Problem父类
         return referenceObjV
 
     def aimFunc(self, pop):  # 目标函数
-        Xijbr = pop.Phen.reshape([40,B,R,S,T])  # 得到决策变量矩阵
+        Xijbr = pop.Phen.reshape([Nind,B,R,S,T])  # 得到决策变量矩阵
 
         # 式5
-        preCV6 = Xijbr.sum(axis=(1, 2)) - np.ones((40,3, 3), dtype=np.int)  # b*r
-        CV6=np.zeros((40,1), dtype=np.int)
-        for q in range(40):
+        preCV6 = Xijbr.sum(axis=(1, 2)) - np.ones((Nind,B, R), dtype=np.int)  # b*r
+        CV6=np.zeros((Nind,1), dtype=np.int)
+        for q in range(Nind):
             for b in range(B):
                 for r in range(R):
                     if preCV6[q][b][r]>0:
@@ -47,7 +45,7 @@ class MyProblem(ea.Problem):  # 继承Problem父类
                         CV6[q] += preCV6[q][b][r]
 
         # #式3
-        Ttobr=np.zeros((40,3,3), dtype=np.int)          # b*r
+        Ttobr=np.zeros((Nind,B,R), dtype=np.int)          # b*r
         for b in range(B):
             for r in range(R):
                 for i in range(S):
@@ -58,32 +56,28 @@ class MyProblem(ea.Problem):  # 继承Problem父类
         TbackbrMin=np.zeros((Nind, B, R-1), dtype=np.int)       # b*(r-1)
         for b in range(B):
             for r in range(R-1):
-                TempTable = np.zeros((40, S, T), dtype=np.int)
+                TempTable = np.zeros((Nind, S, T), dtype=np.int)
                 for i in range(S):
                     for j in range(T):
-                        TempTable[:,[i],[j]]=d[i][j]*(Xijbr[:,:,[j],[b],[r]].sum(axis=1)+Xijbr[:,[i],:,[b],[r+1]].reshape(40,3).sum(axis=1).reshape(40,1)-np.ones((40,1), dtype=np.int))
+                        TempTable[:,[i],[j]]=d[i][j]*(Xijbr[:,:,[j],[b],[r]].sum(axis=1)+Xijbr[:,[i],:,[b],[r+1]].reshape(Nind,T).sum(axis=1).reshape(Nind,1)-np.ones((Nind,1), dtype=np.int))
                 for q in range(Nind):
                     TbackbrMin[q][b][r]=TempTable[q].max()
-
-
-
 
         #式2
         TmaxMin=Ttobr.sum(axis=(2))+TbackbrMin.sum(axis=(2))        #40*b(40*3)
         for i in range(S):
             for j in range(T):
-                TmaxMin+=dStart[i]*Xijbr[:,[i],[j],:,[0]].reshape(40,3)
+                TmaxMin+=dStart[i]*Xijbr[:,[i],[j],:,[0]].reshape(Nind,B)
 
         #式6
-
-        preCV3 = np.zeros((40, 3, 3), dtype=np.int)  # b*(r-1)
+        preCV3 = np.zeros((Nind, B, R), dtype=np.int)  # b*(r-1)
         for b in range(B):
             for r in range(R - 1):
                 for i in range(S):
                     for j in range(T):
                         preCV3[:, [b], [r]] += Xijbr[:, [i], [j], [b], [r + 1]] - Xijbr[:, [i], [j], [b], [r]]
-        CV3 = np.zeros((40, 1), dtype=np.int)
-        for q in range(40):
+        CV3 = np.zeros((Nind, 1), dtype=np.int)
+        for q in range(Nind):
             # cnt=0
             for b in range(B):
                 for r in range(R):
@@ -91,36 +85,35 @@ class MyProblem(ea.Problem):  # 继承Problem父类
                         CV3[q]+=preCV3[q][b][r]
 
         #式7
-        preCV4 =np.stack([np.array(L) for _ in range(40)], axis=0)
-        preCV4[:, :] -= Xijbr.sum(axis=(2, 3, 4)).reshape(40, 3)
-        CV4 = np.zeros((40, 1), dtype=np.int)
-        for q in range(40):
+        preCV4 =np.stack([np.array(L) for _ in range(Nind)], axis=0)
+        preCV4[:, :] -= Xijbr.sum(axis=(2, 3, 4)).reshape(Nind, S)
+        CV4 = np.zeros((Nind, 1), dtype=np.int)
+        for q in range(Nind):
             for i in range(S):
                 if preCV4[q][i] > 0:
                     CV4[q] += preCV4[q][i]
 
         #式8
-        preCV5 = np.stack([np.array(U) for _ in range(40)], axis=0)
-        preCV5[:, :] -= Xijbr.sum(axis=(1, 3, 4)).reshape(40, 3)
-        CV5 = np.zeros((40, 1), dtype=np.int)
-        for q in range(40):
+        preCV5 = np.stack([np.array(U) for _ in range(Nind)], axis=0)
+        preCV5[:, :] -= Xijbr.sum(axis=(1, 3, 4)).reshape(Nind, T)
+        CV5 = np.zeros((Nind, 1), dtype=np.int)
+        for q in range(Nind):
             for j in range(T):
                 if preCV5[q][j] < 0:
                     CV5[q] -= preCV5[q][j]
 
-        pop.ObjV = np.max(TmaxMin, axis=1).reshape(40, 1)  # 计算目标函数值，赋值给pop种群对象的ObjV属性
+        pop.ObjV = np.max(TmaxMin, axis=1).reshape(Nind, 1)  # 计算目标函数值，赋值给pop种群对象的ObjV属性
         pop.CV=np.hstack([CV3,
                           CV4,
                           CV5,
                           CV6])
-
 
 if __name__ == '__main__':
     """===============================实例化问题对象==========================="""
     problem = MyProblem()  # 生成问题对象
     """=================================种群设置=============================="""
     Encoding = 'BG'  # 编码方式
-    NIND = 40  # 种群规模
+    NIND = Nind  # 种群规模
     Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
     population = ea.Population(Encoding, Field, NIND)  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
     """===============================算法参数设置============================="""
@@ -161,7 +154,7 @@ if __name__ == '__main__':
         print('验证式7：L=[1,3,3],Xijbr(sum)=',Xijbr.sum(axis=(1,2,3)))
         print('验证式6：左侧=', Xijbr.sum(axis=(0,1))[:,[0,1]],'右侧=', Xijbr.sum(axis=(0,1))[:,[1,2]])
         print('验证式5：左侧=', Xijbr.sum(axis=(0,1)))
-        Ttobr = np.ones((3, 3), dtype=np.int)
+        Ttobr = np.ones((B, R), dtype=np.int)
         for b in range(B):
             for r in range(R):
                 Ttobr[b, r] = (d * Xijbr[:, :, b, r]).sum(axis=(0,1))
